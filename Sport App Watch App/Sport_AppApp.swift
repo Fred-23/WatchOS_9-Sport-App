ba750dd5a6d82044bd0bce47048514c1fde92bec
Frédéric <alphonse.frederic23@icloud.com>
2022-10-11 09:30:38 +0200

    Initial Commit

---
+++ Sport App Watch App/Sport_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sport_AppApp.swift
+//  Sport App Watch App
+//
+//  Created by Frédéric ALPHONSE on 11/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct Sport_App_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
